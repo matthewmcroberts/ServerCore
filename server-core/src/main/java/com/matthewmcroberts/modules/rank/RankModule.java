8c1acf7381dd786bc0b5338dc9ed1c51796e3f4a
Matthew <mm20captingoof@gmail.com>
2026-09-05 20:04:35 -0400

    feat: create rank

--- server-core/src/main/java/com/matthewmcroberts/modules/rank/RankModule.java
+++ server-core/src/main/java/com/matthewmcroberts/modules/rank/RankModule.java
@@ -4,16 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.matthewmcroberts.modules.ServerModule;
 import com.matthewmcroberts.modules.rank.client.RankApiClient;
 import com.matthewmcroberts.modules.rank.client.RankWebSocketClient;
+import com.matthewmcroberts.modules.rank.models.Rank;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 import static org.bukkit.Bukkit.getLogger;
 
 @RequiredArgsConstructor
 public class RankModule implements ServerModule {
     private final JavaPlugin plugin;
+
+    private List<Rank> ranksCache = new ArrayList<>();
 
     private RankWebSocketClient webSocketClient;
     private RankApiClient apiClient;
@@ -31,6 +39,8 @@ public class RankModule implements ServerModule {
                 "ws://localhost:8080/ws"
         );
 
+        this.getAllRanks();
+
         getLogger().info("RankModule setup complete.");
     }
 
@@ -46,8 +56,42 @@ public class RankModule implements ServerModule {
     }
 
     // createRank
+    public @NonNull CompletableFuture<Rank> createRank(
+            @NonNull final String rankId,
+            @NonNull final String rawDisplayName,
+            final int priority,
+            @NonNull final Set<String> ownPermissions,
+            @NonNull final Set<String> effectivePermissions,
+            @NonNull final Set<String> inheritedRankIds) {
+        final Rank newRank = Rank.builder()
+                .id(rankId)
+                .displayName(rawDisplayName)
+                .priority(priority)
+                .ownPermissions(ownPermissions)
+                .effectivePermissions(effectivePermissions)
+                .inheritedRankIds(inheritedRankIds)
+                .build();
+
+        return apiClient.createRank(newRank).thenApply(rank -> {
+            this.ranksCache.add(rank);
+            return rank;
+        });
+    }
+
     // getRankById
     // getAllRanks
+    public CompletableFuture<List<Rank>> getAllRanks() {
+        if(!ranksCache.isEmpty()) {
+            return CompletableFuture.completedFuture(List.copyOf(ranksCache));
+        }
+
+        return apiClient.getAllRanks().thenApply(ranks -> {
+            ranksCache.clear();
+            ranksCache.addAll(ranks);
+            return List.copyOf(ranks);
+        });
+    }
+
     // updateRankDisplayName
     // updateRankPriority
     // addRankPermissions
